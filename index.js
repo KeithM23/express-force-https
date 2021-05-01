a38389f10c07ca2843ab712cbc0d44252f2e698d
KeithM23 <36164461+KeithM23@users.noreply.github.com>
2021-05-01 08:32:33 +0100

    Redirect as 301 instead of 302

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 module.exports = function(req,res,next) {
   var schema = (req.headers['x-forwarded-proto'] || '').toLowerCase();
   if (req.headers.host.indexOf('localhost')<0 && schema!=='https') {
-    res.redirect('https://' + req.headers.host + req.url);
+    res.redirect(301, 'https://' + req.headers.host + req.url);
   } else {
     next();
   }
